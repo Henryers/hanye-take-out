a5116f77f338398103722b9923b3d9bd8941c48b
cyh <cyhrecold@outlook.com>
2025-05-12 21:15:20 +0800

    fix comment

--- hanye-take-out-springboot3/common/src/main/java/fun/cyhgraph/result/Result.java
+++ hanye-take-out-springboot3/common/src/main/java/fun/cyhgraph/result/Result.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 @Data
 public class Result<T> implements Serializable {
 
-    private Integer code; //编码：1成功，0和其它数字为失败
+    private Integer code; //编码：0成功，1和其它数字为失败
     private String msg; //错误信息
     private T data; //数据
 
